3e8726db0f1467eb375703ff8f1a93da2e0e961b
lolito25 <68433235+lolito25@users.noreply.github.com>
2022-02-15 00:03:13 -0500

    Add files via upload

---
+++ carta.py
@@ -0,0 +1,28 @@
+nombre = input("Escribe tu nombre: ")
+
+print("Buenos dias " + nombre + ", por favor responde a las siguientes preguntas ")
+
+
+años = int(input("¿Cuantos años tienes?: "))
+nacio = input("¿En que parte de Colombia vives?: ")
+
+
+como_se_ve = input("1. " + nombre + " ¿Como te vez en 4 años laburando como cientifico de datos?: ")
+vivir = input ("2. ¿En donde te imaginas viviendo con esta profesion?: ")
+ganancias = input("3. ¿Cuanto aspiras ganar?: ")
+love = input("¿Trabajarias gratis en la empresa del profesor Rippe solo por amor a lo que haces? SI-NO , ¿por que?")
+print("Muchas gracias" + nombre + "por tu tiempo, cuidate las tapas")
+
+estudiante = "Santiago Florez"
+materia = float(input("¿Profesor Rippe cuanto le coloca a esta Carta del estudiante " + estudiante + "?: "))
+
+promedio = (materia) / 3
+promedio = float(materia)
+
+if promedio >= 3:
+    print("Felicidades " + estudiante + " APROBO el trabajo con un promedio de:", promedio)
+
+print( estudiante , "Ya puede seguir durmiendo " )
+
+
+
